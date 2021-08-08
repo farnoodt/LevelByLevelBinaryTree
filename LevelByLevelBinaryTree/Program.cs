909b5880f762ff4fb8c6098203c402f392a9a1a3
Farnoud Taghdissi Borhani <Farnood_t@yahoo.com>
2021-08-08 01:41:22 -0700

    Add project files.

---
+++ LevelByLevelBinaryTree/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace LevelByLevelBinaryTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Node tree = new Node(1);
+            tree.left = new Node(2);
+            tree.left.left = new Node(4);
+            tree.left.right = new Node(5);
+            tree.left.right.left = new Node(8);
+            tree.left.right.right = new Node(9);
+            tree.right = new Node(3);
+            tree.right.left = new Node(6);
+            tree.right.right = new Node(7);
+            tree.right.right.left = new Node(10);
+
+            TraversalLevelByLevel LBL = new TraversalLevelByLevel();
+            LBL.printLevelbyLevel(tree);
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ LevelByLevelBinaryTree/TraversalLevelByLevel.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LevelByLevelBinaryTree
+{
+    class TraversalLevelByLevel
+    {
+       public void  printLevelbyLevel(Node root)
+        {
+            if (root == null)
+                return;
+
+            Queue<Node> Q1 = new Queue<Node>();
+            Queue<Node> Q2 = new Queue<Node>();
+
+            Q1.Enqueue(root);
+
+            while (Q1.Count != 0 || Q2.Count != 0)
+            {
+                Console.WriteLine();
+                if(Q1.Count != 0)
+                {
+                    while(Q1.Count != 0)
+                    {
+                        root = Q1.Dequeue();
+                        Console.Write(root.data + ", ");
+                        if (root.left != null)
+                            Q2.Enqueue(root.left);
+
+                        if (root.right != null)
+                            Q2.Enqueue(root.right);
+                    }
+                }
+
+                Console.WriteLine();
+                if(Q2.Count != 0)
+                {
+                    while(Q2.Count != 0)
+                    {
+                        root = Q2.Dequeue();
+                        Console.Write(root.data + ", ");
+                        if (root.left != null)
+                            Q1.Enqueue(root.left);
+
+                        if (root.right != null)
+                            Q1.Enqueue(root.right);
+                    }
+                }
+            }
+        }
+    }
+}
